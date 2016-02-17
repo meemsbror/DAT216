5c8a869a2df7d7509f2a19de623c7072a388a99d
Simon Moos <simon.moos95@gmail.com>
2016-02-17 14:07:20 +0100

    Give root view border layout

--- iMat/src/imat/RootViewController.java
+++ iMat/src/imat/RootViewController.java
@@ -1,8 +1,12 @@
 package imat;
 
 import imat.viewcontroller.ViewController;
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
 
 public class RootViewController extends ViewController {
+
+    @FXML private BorderPane borderPane;
 
     @Override
     public void initialize() {
