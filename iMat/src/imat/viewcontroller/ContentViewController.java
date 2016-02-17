428f5cf61abe13fef7c93a1f1a5dc42ac3d6760a
Frej Karlsson <frejlekarlsson@gmail.com>
2016-02-17 15:04:17 +0100

    added categoryButtons

--- iMat/src/imat/viewcontroller/ContentViewController.java
+++ iMat/src/imat/viewcontroller/ContentViewController.java
@@ -2,9 +2,6 @@ package imat.viewcontroller;
 
 import imat.viewcontroller.ViewController;
 
-/**
- * Created by frej on 2/17/16.
- */
 public abstract  class ContentViewController extends ViewController{
 
 }
--- iMat/src/imat/viewcontroller/RootViewController.java
+++ iMat/src/imat/viewcontroller/RootViewController.java
@@ -2,8 +2,11 @@ package imat.viewcontroller;
 
 import imat.viewcontroller.ContentViewController;
 import imat.viewcontroller.ViewController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+import se.chalmers.ait.dat215.project.ProductCategory;
 
 public class RootViewController extends ViewController {
 
@@ -19,9 +22,16 @@ public class RootViewController extends ViewController {
     public void setContent(ContentViewController c){
 
         this.content=c;
+        ProductCategory pcs [] = ProductCategory.values();
+        for(ProductCategory ps:pcs){
+            categoryButton btn = new categoryButton(ps.name());
+            btn.setOnAction((e) -> productCategorySelected(e));
+        }
 
+    }
 
-
+    private void productCategorySelected(ActionEvent evt){
+        //TODO: Choose category and show
     }
 
 }
