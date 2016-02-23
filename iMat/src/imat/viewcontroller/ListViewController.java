09612e557090b4556f04487a3ffd2fc22e89fa6b
Simon Moos <simon.moos95@gmail.com>
2016-02-23 18:07:31 +0100

    Implement a first draft to list view

--- iMat/src/imat/viewcontroller/ListViewController.java
+++ iMat/src/imat/viewcontroller/ListViewController.java
@@ -5,23 +5,37 @@ import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.layout.FlowPane;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+import se.chalmers.ait.dat215.project.Product;
 
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ListViewController extends ContentViewController {
 
-    private enum SortingDescriptor {
+    private enum SortDescriptor {
         ALPHABETICALLY_ASCENDING,
         ALPHABETICALLY_DESCENDING,
         PRICE_ASCENDING,
         PRICE_DESCENDING
     }
 
+    private List<Product> products = new ArrayList<>();
+
     private ProductFilter productFilter;
-    private SortingDescriptor sortingDescription;
+    private SortDescriptor sortDescription;
 
     @FXML private Label productFilterNameLabel;
-    @FXML private RadioButton defaultRadioButton;
+
+    @FXML private RadioButton aToOsortRadioButton;
+    @FXML private RadioButton oToAsortRadioButton;
+    @FXML private RadioButton lowToHighSortRadioButton;
+    @FXML private RadioButton highToLowSortRadioButton;
+
+    @FXML private FlowPane flowPane;
 
     @Override
     public void initialize() {
@@ -36,8 +50,52 @@ public class ListViewController extends ContentViewController {
         this.productFilter = productFilter;
         productFilterNameLabel.setText(productFilter.getName());
 
+        // Fetch & filter products
+        this.products = productFilter.filterProducts(IMatDataHandler.getInstance().getProducts());
+
         // Reset the sorting radio buttons
-        defaultRadioButton.setSelected(true);
+        aToOsortRadioButton.setSelected(true);
+        setSortDescriptorAndPerformSort(SortDescriptor.ALPHABETICALLY_ASCENDING);
+    }
+
+    private void setSortDescriptorAndPerformSort(SortDescriptor sortDescriptor) {
+        this.sortDescription = sortDescriptor;
+        Collections.sort(this.products, (o1, o2) -> {
+            switch (sortDescription) {
+
+                case ALPHABETICALLY_ASCENDING:
+                    return o1.getName().compareTo(o2.getName());
+
+                case ALPHABETICALLY_DESCENDING:
+                    return o2.getName().compareTo(o1.getName());
+
+                case PRICE_ASCENDING:
+                    return (int)(o1.getPrice() - o2.getPrice());
+
+                case PRICE_DESCENDING:
+                    return (int)(o2.getPrice() - o1.getPrice());
+
+                default:
+                    return 0;
+            }
+        });
+
+        displayProducts(this.products);
+    }
+
+    private void displayProducts(List<Product> productsToDisplay) {
+
+        // First empty out old products
+        flowPane.getChildren().clear();
+
+        for (Product product: productsToDisplay) {
+
+            // TODO: Pool & reuse for performance and load time!
+            ProductTileViewController tile = ProductTileViewController.load("ProductTileView.fxml");
+            tile.setProduct(product);
+
+            flowPane.getChildren().add(tile.getView());
+        }
     }
 
     @FXML
@@ -45,15 +103,12 @@ public class ListViewController extends ContentViewController {
         if (event.getSource() instanceof RadioButton) {
             RadioButton radioButton = (RadioButton) event.getSource();
 
-            String name = radioButton.getText();
-            // TODO: Set sorting based on the radio buttons!
+            // Set sorting descriptor & sort list
+            if (radioButton.equals(aToOsortRadioButton)) setSortDescriptorAndPerformSort(SortDescriptor.ALPHABETICALLY_ASCENDING);
+            if (radioButton.equals(oToAsortRadioButton)) setSortDescriptorAndPerformSort(SortDescriptor.ALPHABETICALLY_DESCENDING);
+            if (radioButton.equals(lowToHighSortRadioButton)) setSortDescriptorAndPerformSort(SortDescriptor.PRICE_ASCENDING);
+            if (radioButton.equals(highToLowSortRadioButton)) setSortDescriptorAndPerformSort(SortDescriptor.PRICE_DESCENDING);
         }
     }
-
-    public void performSorting() {
-        // TODO: Sort list based on the sortingDescription!
-    }
-
-    
 
 }
