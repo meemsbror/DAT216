72272bc946a89efb2782adc357817f801b04b31e
rebeccafinne <rebecca@finne.se>
2016-02-24 11:05:49 +0100

    Trying to fix nullpointerexception

--- iMat/src/imat/viewcontroller/DetailViewController.java
+++ iMat/src/imat/viewcontroller/DetailViewController.java
@@ -2,6 +2,7 @@ package imat.viewcontroller;
 
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import se.chalmers.ait.dat215.project.IMatDataHandler;
@@ -26,16 +27,20 @@ public class DetailViewController extends ContentViewController {
 
     }
 
-
-
-
     public void setTitle(Product product){
-        productName.setText(ProductTileViewController.getProduct().getName());
+        productName.setText(product.getName());
     }
 
     public void setPrice(Product product){
-        double price = product.getPrice();
-        productPrice.setText(String.valueOf(price));
+        //product = IMatDataHandler.getInstance().getProduct(product.getProductId());
+        productPrice.setText(String.valueOf(product.getPrice()));
+
+    }
+
+    public void setProductImage(Product product){
+        Image detailImage = IMatDataHandler.getInstance().getFXImage(product, 247, 214);
+        productImage.setImage(detailImage);
+
     }
 
 }
