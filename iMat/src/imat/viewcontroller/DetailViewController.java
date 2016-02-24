e030d0cbb0ac8091ccec563de4a2ddfa770d2b1d
rebeccafinne <rebecca@finne.se>
2016-02-24 10:23:00 +0100

    setTitle,  setPrice and getProduct created

--- iMat/src/imat/viewcontroller/DetailViewController.java
+++ iMat/src/imat/viewcontroller/DetailViewController.java
@@ -1,11 +1,19 @@
 package imat.viewcontroller;
 
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+import se.chalmers.ait.dat215.project.Product;
 
-/**
- * Created by rebeccafinne on 16-02-24.
- */
+
 public class DetailViewController extends ContentViewController {
+
+    @FXML private ImageView productImage;
+    @FXML private Text productName;
+    @FXML private Text productPrice;
+
 
 
     @Override
@@ -17,4 +25,17 @@ public class DetailViewController extends ContentViewController {
     protected void viewDidSet(Parent view) {
 
     }
+
+
+
+
+    public void setTitle(Product product){
+        productName.setText(ProductTileViewController.getProduct().getName());
+    }
+
+    public void setPrice(Product product){
+        double price = product.getPrice();
+        productPrice.setText(String.valueOf(price));
+    }
+
 }
