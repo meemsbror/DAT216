55d46f628577fc2b49b2347bab4b3317ed4b0ac8
rebeccafinne <rebecca@finne.se>
2016-03-04 09:39:10 +0100

    Chnged the smallProductView

--- iMat/src/imat/viewcontroller/SmallHistoryProductViewController.java
+++ iMat/src/imat/viewcontroller/SmallHistoryProductViewController.java
@@ -1,7 +1,18 @@
 package imat.viewcontroller;
 
+import javafx.scene.Parent;
+
 /**
  * Created by Kotex on 02/03/2016.
  */
-public class SmallHistoryProductViewController {
+public class SmallHistoryProductViewController extends ViewController {
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    protected void viewDidSet(Parent view) {
+
+    }
 }
